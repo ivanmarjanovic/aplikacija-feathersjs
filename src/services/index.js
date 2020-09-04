668ee4112876082f124164c33cb21d4316e44366
Nikola <nikola.kilibarda85@gmail.com>
2020-09-04 15:23:42 +0200

    commit for upload

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const messages = require('./messages/messages.service.js');
+const users = require('./users/users.service.js');
+const posts = require('./posts/posts.service.js');
+// eslint-disable-next-line no-unused-vars
+module.exports = function (app) {
+  app.configure(messages);
+  app.configure(users);
+  app.configure(posts);
+};
