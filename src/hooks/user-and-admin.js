7bce95491afa401b58db481373b4986cccee3d58
Nikola <nikola.kilibarda85@gmail.com>
2020-09-30 19:50:39 +0200

    admin i user radi

--- src/hooks/user-and-admin.js
+++ src/hooks/user-and-admin.js
@@ -3,46 +3,9 @@
 const { Forbidden } = require('@feathersjs/errors');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = (options = {}) => {
-  return async context => {
-    
-    /*const restrictUser =*/ async context => {
-    const { user } = context.params;
-    
-    if(user.roles.includes('admin')) {
-    return context;
-    }
-    if(!context.id) {
-    context.params.query._id = user._id;
-    } else {
-    const item = await context.service.get(context.userId);
-    if(item.userId !== user._id) {
-    throw new Forbidden('You are not allowed to access this');
-    }
-    }
-    
-    return context;
-    };
-  };
-};
 
-// const { Forbidden } = require('@feathersjs/errors'); 
-// module.exports = (options = {}) => {
-//   return async context => {   
-//           const { user } = context.params;
-          
-//           if(user.roles.includes('admin') || data.params.userId.equals(user.params.userId) {
-//             // return context;
-//           }
-//           const { data } = context;
-//           if (!context.id) {
-//             context.params.query._id = user._id
-//           }else {
-//             const post = await context.service.get(context.user_Id);
-//             if(post.userId !== user._id) {
-//             throw new Forbidden('You are not allowed to access this');
-//             }
-//           }
-//           return context;
-//   };
-// };
+
+
+module.exports = (options = {}) => {
+  
+};
--- src/services/posts/posts.hooks.js
+++ src/services/posts/posts.hooks.js
@@ -1,11 +1,10 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
-const userAndAdmin = require('../../hooks/user-and-admin');
-
 const processPost = require('../../hooks/process-post');
 
-
 const wordCount = require('../../hooks/word-count');
+
+const { Forbidden } = require('@feathersjs/errors');
 
 
 module.exports = {
@@ -16,7 +15,7 @@ module.exports = {
     create: [processPost()],
     update: [userAndAdmin],
     patch: [userAndAdmin],
-    remove: [userAndAdmin()]
+    remove: [userAndAdmin]
   },
 
   after: {
@@ -38,4 +37,29 @@ module.exports = {
     patch: [],
     remove: []
   }
+};
+async function userAndAdmin(context) {   
+  try {const user = context.params.user;
+  const data = context.data;
+  console.log('user je ' + JSON.stringify(user));
+  console.log('date je ' + JSON.stringify(data));
+  if(user.roles !== undefined && user.roles.includes('admin')) {
+    return context;
+  }
+  // if(user.roles.includes('admin') || data.params.userId.equals(user.params.userId)) {
+  //   return context;
+  // }
+  
+  if (!context.id) {
+    throw new Forbidden('Ne moze vise ovako!');
+  }else {
+    const post = await context.service.get(context.id);
+    if(!post.userId.equals(user._id)) {
+    throw new Forbidden('You are not allowed to access this');
+    }
+  }
+  return context;}
+  catch(error){
+    console.log(error.stack);
+    throw error;};
 };
